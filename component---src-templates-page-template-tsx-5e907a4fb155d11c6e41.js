05f84b6c08c5f9474fab3ffe0636770b9c39456b
trung.nguyen <trung.nguyen@crystal-techs.com>
2020-08-04 13:14:21 +0700

    - Sprint2

--- component---src-templates-page-template-tsx-5e907a4fb155d11c6e41.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{sssF:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),c=a("Wbzz"),l=a("Ac47"),r=a("PIHY");t.default=function(e){return o.a.createElement(l.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2 (",e.path,")"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-templates-page-template-tsx-5e907a4fb155d11c6e41.js.map
