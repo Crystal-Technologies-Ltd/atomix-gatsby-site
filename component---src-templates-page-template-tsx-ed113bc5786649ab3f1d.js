ee7ac101581f030a688016b74c09ba339a84d4ed
trungnguyen-crystal <trung.nguyen@crystal-techs.com>
2020-08-12 18:10:32 +0000

    Auto deploy 2020-08-12 18:10:32

---
+++ component---src-templates-page-template-tsx-ed113bc5786649ab3f1d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{sssF:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),c=a("Wbzz"),l=a("Ac47"),r=a("PIHY");t.default=function(e){return o.a.createElement(l.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2 (",e.path,")"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-templates-page-template-tsx-ed113bc5786649ab3f1d.js.map
